56cfc7440c9055d1bb3aa480e93e7a124eb83102
jyun9504 <jyun9504@gmail.com>
2020-05-07 14:20:37 +0800

    v1 finish

--- src/store/state.js
+++ src/store/state.js
@@ -3,8 +3,8 @@ const state = {
   [
     { 
       id: 1,
-      name: "青茶",
-      description: "無糖少冰",
+      name: "高山茶",
+      description: "無糖少冰，不是最高山的不喝，Id duis sint in sint veniam occaecat eiusmod esse sit magna officia mollit ea. Ullamco veniam quis amet deserunt id culpa tempor. Occaecat quis occaecat nostrud culpa eu Lorem mollit et in et minim. Sint officia id ea aute do amet aute eiusmod eu dolor ipsum. Sint veniam Lorem excepteur velit quis ad esse sint esse esse. Aliquip ex incididunt qui sunt. Excepteur non aliquip sint dolor id velit adipisicing laboris.",
       price: 20,
       quantity: 1
     }
